6599e1b14fbe7ebc628c5da2ee401454962d2e13
Hamashi <hamashii@gmail.com>
2017-12-08 00:35:07 +0100

    Components créés

---
+++ src/app/vignettes/vignettes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vignettes',
+  templateUrl: './vignettes.component.html',
+  styleUrls: ['./vignettes.component.css']
+})
+export class VignettesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
