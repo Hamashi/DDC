37bee04f2ac5a89e161f24a08a8ffd5679a3d24a
Administrator <vgilles@et.esiea.fr>
2017-12-08 03:25:54 +0100

    [ADD] angular material added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 import { VignettesComponent } from './vignettes/vignettes.component';
 import { ModaleComponent } from './modale/modale.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     VignettesComponent,
     ModaleComponent
-  ],
+  ],	
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
